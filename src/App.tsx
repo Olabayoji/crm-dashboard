78366969a3fbee17c1ea0dc9519793fe1500e623
olabayoji <bayoji7@gmail.com>
2024-03-03 22:02:40 +0000

    remove banner

--- src/App.tsx
+++ src/App.tsx
@@ -39,7 +39,6 @@ import TasksEditPage from "./pages/tasks/edit";
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner />
       <RefineKbarProvider>
         <AntdApp>
           <DevtoolsProvider>
